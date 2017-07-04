12cebcd1905b86abf0fbcedb60b8f4ddf7155683
Pavel Kozlov <pashasdead@gmail.com>
2017-07-04 12:51:34 +0300

    2.1

--- ch2/cf/main.go
+++ ch2/cf/main.go
@@ -11,8 +11,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-
-	"gopl.io/ch2/tempconv"
+	"gobook/ch2/tempconv"
 )
 
 func main() {
@@ -24,8 +23,9 @@ func main() {
 		}
 		f := tempconv.Fahrenheit(t)
 		c := tempconv.Celsius(t)
-		fmt.Printf("%s = %s, %s = %s\n",
-			f, tempconv.FToC(f), c, tempconv.CToF(c))
+		k := tempconv.Kelvin(t)
+		fmt.Printf("%s = %s, %s = %s, %s = %s\n",
+			f, tempconv.FToC(f), c, tempconv.CToF(c), k, tempconv.KToC(k))
 	}
 }
 
