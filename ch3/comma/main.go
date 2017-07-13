ce81740a6af78fe692f9cfa1b16d4f4c15baffa7
Pavel Kozlov <pashasdead@gmail.com>
2017-07-13 11:51:21 +0300

    3.10

--- ch3/comma/main.go
+++ ch3/comma/main.go
@@ -19,11 +19,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"bytes"
 )
 
 func main() {
 	for i := 1; i < len(os.Args); i++ {
-		fmt.Printf("  %s\n", comma(os.Args[i]))
+		fmt.Printf("  %s\n", commaBuf(os.Args[i]))
 	}
 }
 
@@ -35,6 +36,26 @@ func comma(s string) string {
 		return s
 	}
 	return comma(s[:n-3]) + "," + s[n-3:]
+}
+
+func commaBuf(s string) string {
+	var buf bytes.Buffer
+	var j = 0
+	var count = len(s) / 3
+	var place = len(s) % 3
+	for i := 0; i < len(s); i++ {
+		// fmt.Printf("place = %d j = %d i = %d\n", place, j, i)
+		if count > 0 && place == i  && place != 0 {
+			buf.WriteString(",")
+			j = 0
+		} else if j >= 3 && count > 0 {
+			buf.WriteString(",")
+			j = 0
+		}
+		j++
+		buf.WriteByte(s[i])
+	}
+	return buf.String()
 }
 
 //!-
