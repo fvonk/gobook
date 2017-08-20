ef290207c8eea57948700540b72470b428d034d8
Pavel Kozlov <pashasdead@gmail.com>
2017-08-20 20:20:18 +0300

    7.1

--- ch7/bytecounter/main.go
+++ ch7/bytecounter/main.go
@@ -8,6 +8,8 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"strings"
 )
 
 //!+bytecounter
@@ -21,15 +23,51 @@ func (c *ByteCounter) Write(p []byte) (int, error) {
 
 //!-bytecounter
 
+type WordCounter int
+
+func (c *WordCounter) Write(p []byte) (int, error) {
+	str := string(p)
+	fmt.Println(str)
+	scanner := bufio.NewScanner(strings.NewReader(str))
+	scanner.Split(bufio.ScanWords)
+	count := 0
+	for scanner.Scan() {
+		count++
+	}
+	if err := scanner.Err(); err != nil {
+		return 0, err
+	}
+	*c += WordCounter(count)
+	return len(p), nil
+}
+
+type LineCounter int
+
+func (c *LineCounter ) Write(p []byte) (int, error) {
+	str := string(p)
+	fmt.Println(str)
+	scanner := bufio.NewScanner(strings.NewReader(str))
+	scanner.Split(bufio.ScanLines)
+	count := 0
+	for scanner.Scan() {
+		count++
+	}
+	if err := scanner.Err(); err != nil {
+		return 0, err
+	}
+	*c += LineCounter(count)
+	return len(p), nil
+}
+
 func main() {
 	//!+main
-	var c ByteCounter
-	c.Write([]byte("hello"))
+	var c LineCounter
+	c.Write([]byte("asd"))
 	fmt.Println(c) // "5", = len("hello")
 
 	c = 0 // reset the counter
 	var name = "Dolly"
-	fmt.Fprintf(&c, "hello, %s", name)
+	fmt.Fprintf(&c, "hel \nlo lo, \n%s", name)
 	fmt.Println(c) // "12", = len("hello, Dolly")
 	//!-main
 }
