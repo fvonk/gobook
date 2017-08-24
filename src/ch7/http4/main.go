65dfe058eff8df4a8c4f7d3735900241d6b1de0d
Pavel Kozlov <pavelkozlov@pavel.local>
2017-08-24 15:30:49 +0300

    7.11

--- src/ch7/http4/main.go
+++ src/ch7/http4/main.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 //!+main
@@ -19,6 +20,9 @@ func main() {
 	db := database{"shoes": 50, "socks": 5}
 	http.HandleFunc("/list", db.list)
 	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/update", db.update)
+	http.HandleFunc("/create", db.create)
+	http.HandleFunc("/remove", db.remove)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
@@ -40,6 +44,49 @@ func (db database) price(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
 	if price, ok := db[item]; ok {
 		fmt.Fprintf(w, "%s\n", price)
+	} else {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+	}
+}
+
+func (db database) update(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, err := strconv.ParseFloat(req.URL.Query().Get("price"), 32)
+	if err != nil {
+		fmt.Fprintf(w, "new price is not correct %q\n", err)
+	} else {
+		if _, ok := db[item]; ok {
+			db[item] = dollars(price)
+			fmt.Fprintf(w, "new price %v for %s udpated\n", price, item)
+		} else {
+			w.WriteHeader(http.StatusNotFound) // 404
+			fmt.Fprintf(w, "no such item: %q\n", item)
+		}
+	}
+}
+
+func (db database) create(w http.ResponseWriter, req *http.Request) {
+	newItem := req.URL.Query().Get("item")
+	newPrice, err := strconv.ParseFloat(req.URL.Query().Get("price"), 32)
+	if err != nil {
+		fmt.Fprintf(w, "new price is not correct %q\n", err)
+	} else {
+		if _, ok := db[newItem]; ok {
+			fmt.Fprintf(w, "new item %v already exists\n", newItem)
+		} else {
+			db[newItem] = dollars(newPrice)
+			fmt.Fprintf(w, "new price %v for %s created\n", newPrice, newItem)
+		}
+
+	}
+}
+
+func (db database) remove(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	if _, ok := db[item]; ok {
+		delete(db, item)
+		fmt.Fprintf(w, "item %s is removed\n", item)
 	} else {
 		w.WriteHeader(http.StatusNotFound) // 404
 		fmt.Fprintf(w, "no such item: %q\n", item)
--- src/ch7/sorting/main.go
+++ src/ch7/sorting/main.go
@@ -101,6 +101,21 @@ func main() {
 	}})
 	//!-customcall
 	printTracks(tracks)
+
+	sSort := stableSort{tracks, func(x, y *Track) bool {
+		if x.Title != y.Title {
+			return x.Title < y.Title
+		}
+		if x.Year != y.Year {
+			return x.Year < y.Year
+		}
+		if x.Length != y.Length {
+			return x.Length < y.Length
+		}
+		return false
+	}}
+	sort.Sort(sSort)
+	printTracks(tracks)
 }
 
 /*
@@ -165,3 +180,13 @@ func init() {
 	fmt.Println(sort.IntsAreSorted(values)) // "false"
 	//!-ints
 }
+
+type stableSort struct {
+	t    []*Track
+	order []
+	less func(x, y *Track) bool
+}
+
+func (x customSort) Len() int           { return len(x.t) }
+func (x customSort) Less(i, j int) bool { return x.less(x.t[i], x.t[j]) }
+func (x customSort) Swap(i, j int)      { x.t[i], x.t[j] = x.t[j], x.t[i] }
