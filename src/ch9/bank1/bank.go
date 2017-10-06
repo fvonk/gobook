33e3dac309b22f97aaf15281b5e012582b83eaa9
Pavel Kozlov <pavelkozlov@pavlusha.local>
2017-10-06 15:32:30 +0300

    9.1

--- src/ch9/bank1/bank.go
+++ src/ch9/bank1/bank.go
@@ -7,11 +7,22 @@
 // Package bank provides a concurrency-safe bank with one account.
 package bank
 
+type WithdrawData struct {
+	amount int
+	result chan bool
+}
+
 var deposits = make(chan int) // send amount to deposit
 var balances = make(chan int) // receive balance
+var withdraws = make(chan WithdrawData)
 
 func Deposit(amount int) { deposits <- amount }
 func Balance() int       { return <-balances }
+func Withdraw(amount int) bool {
+	ch := make(chan bool)
+	withdraws <- WithdrawData{amount, ch}
+	return <-ch
+}
 
 func teller() {
 	var balance int // balance is confined to teller goroutine
@@ -20,6 +31,13 @@ func teller() {
 		case amount := <-deposits:
 			balance += amount
 		case balances <- balance:
+		case withdrawData := <- withdraws:
+			if balance >= withdrawData.amount {
+				balance -= withdrawData.amount
+				withdrawData.result <- true
+			} else {
+				withdrawData.result <- false
+			}
 		}
 	}
 }
--- src/ch9/bank1/bank_test.go
+++ src/ch9/bank1/bank_test.go
@@ -7,11 +7,18 @@ import (
 	"fmt"
 	"testing"
 
-	"gopl.io/ch9/bank1"
+	"ch9/bank1"
 )
 
 func TestBank(t *testing.T) {
 	done := make(chan struct{})
+	bank.Deposit(50)
+
+	//go func() {
+	//	bank.Withdraw(50)
+	//	fmt.Println("=", bank.Balance())
+	//	done <- struct{}{}
+	//}()
 
 	// Alice
 	go func() {
@@ -29,8 +36,65 @@ func TestBank(t *testing.T) {
 	// Wait for both transactions.
 	<-done
 	<-done
+	//<-done
 
-	if got, want := bank.Balance(), 300; got != want {
+	if got, want := bank.Balance(), 350; got != want {
+		t.Errorf("Balance = %d, want %d", got, want)
+	}
+
+	if got, want := bank.Withdraw(550), false; got != want {
+		t.Errorf("Withdraw Balance = %t, want %t", got, want)
+	}
+	if got, want := bank.Withdraw(150), true; got != want {
+		t.Errorf("Withdraw Balance = %t, want %t", got, want)
+	}
+
+	if got, want := bank.Balance(), 200; got != want {
+		t.Errorf("Balance = %d, want %d", got, want)
+	}
+}
+
+func TestBank2(t *testing.T) {
+	done := make(chan struct{})
+	bank.Withdraw(200)
+	// Alice
+	go func() {
+		bank.Deposit(200)
+		if ok := bank.Withdraw(20); ok {
+			fmt.Println("Withdraw 20")
+		} else {
+			fmt.Println("Can't withdraw 20")
+		}
+		if ok := bank.Withdraw(400); ok {
+			fmt.Println("Withdraw 400")
+		} else {
+			fmt.Println("Can't withdraw 400")
+		}
+		fmt.Println("=", bank.Balance())
+		done <- struct{}{}
+	}()
+
+	// Bob
+	go func() {
+		bank.Deposit(100)
+		if ok := bank.Withdraw(20); ok {
+			fmt.Println("Withdraw 20")
+		} else {
+			fmt.Println("Can't withdraw 20")
+		}
+		if ok := bank.Withdraw(400); ok {
+			fmt.Println("Withdraw 400")
+		} else {
+			fmt.Println("Can't withdraw 400")
+		}
+		done <- struct{}{}
+	}()
+
+	// Wait for both transactions.
+	<-done
+	<-done
+
+	if got, want := bank.Balance(), 260; got != want {
 		t.Errorf("Balance = %d, want %d", got, want)
 	}
 }
