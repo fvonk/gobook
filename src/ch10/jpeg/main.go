6ea29d2c3f2b95bdf337b1f97450be81905fd976
Pavel Kozlov <pavelkozlov@pavlusha.local>
2017-10-18 00:05:12 +0300

    10.1

--- src/ch10/jpeg/main.go
+++ src/ch10/jpeg/main.go
@@ -13,25 +13,46 @@ import (
 	"fmt"
 	"image"
 	"image/jpeg"
-	_ "image/png" // register PNG decoder
+	"image/png" // register PNG decoder
+	"image/gif"
+	"flag"
 	"io"
 	"os"
 )
 
 func main() {
-	if err := toJPEG(os.Stdin, os.Stdout); err != nil {
-		fmt.Fprintf(os.Stderr, "jpeg: %v\n", err)
+	outputKind := flag.String("output", "jpeg", "")
+	flag.Parse()
+	img, kind, err := image.Decode(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+	fmt.Fprintln(os.Stderr, "Input format =", kind, "Output format =", *outputKind)
+	switch *outputKind {
+	case "jpeg":
+		err = toJPEG(img, os.Stdout)
+	case "png":
+		err = toPNG(img, os.Stdout)
+	case "gif":
+		err = toGif(img, os.Stdout)
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", kind, err)
 		os.Exit(1)
 	}
 }
 
-func toJPEG(in io.Reader, out io.Writer) error {
-	img, kind, err := image.Decode(in)
-	if err != nil {
-		return err
-	}
-	fmt.Fprintln(os.Stderr, "Input format =", kind)
+func toJPEG(img image.Image, out io.Writer) error {
 	return jpeg.Encode(out, img, &jpeg.Options{Quality: 95})
+}
+
+func toPNG(img image.Image, out io.Writer) error {
+	return png.Encode(out, img)
+}
+
+func toGif(img image.Image, out io.Writer) error {
+	return gif.Encode(out, img, &gif.Options{NumColors: 8})
 }
 
 //!-main
