08f79181eb7d9941396494dbefedb441ea7a5caf
Pavel Kozlov <pavelkozlov@pavlusha.local>
2017-12-13 11:36:06 +0300

    11.5

---
+++ src/ch11/11.2/split_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+	"strings"
+)
+
+func TestSplit(t *testing.T) {
+	tests := []struct{
+		s, sep string
+		want int
+	}{
+		{"a:b:c", ":", 3},
+		{"a,b,c,", ",", 4},
+	}
+
+	for _, test := range tests {
+		words := strings.Split(test.s, test.sep)
+		if got := len(words); got != test.want {
+			t.Error("Split(%q, %q) returned %d words, want %d", test.s, test.sep, got, test.want)
+		}
+	}
+}
